113929587cedbf2bf3c3985b4395a53abbeb6ba3
Yanni <1024109095@qq.com>
2019-11-07 22:49:57 +0800

    bmp: 2 16

--- MFCApplication2/MyDIB.cpp
+++ MFCApplication2/MyDIB.cpp
@@ -13,37 +13,60 @@ void DIB1::Read(const CString& fileName) {
 	// 读取 BMPFileHeader
 	file.Read(&m_BMPFileHeader, sizeof(BMPFileHeader));
 	// 读取 BMPInfoHeader
-	ULONGLONG posi = file.Seek(sizeof(BMPFileHeader), CFile::begin);
+	//ULONGLONG posi = file.Seek(sizeof(BMPFileHeader), CFile::begin);
 	file.Read(&m_BMPInfoHeader, sizeof(BMPInfoHeader));
-	posi = file.Seek(sizeof(BMPInfoHeader) + posi, CFile::begin);
+	//posi = file.Seek(sizeof(BMPInfoHeader) + posi, CFile::begin);
 	// read information
 	m_width = m_BMPInfoHeader.biWidth;
 	m_height = m_BMPInfoHeader.biHeight;
 	real_size = m_BMPInfoHeader.biSize;
 	//计算图像每行像素所占的字节数（必须是4的倍数）
 	int biBitCount = m_BMPInfoHeader.biBitCount;
-	int lineByte = (m_width + 3) / 4 * 4;
+	//int lineByte = (m_width + 3) / 4 * 4;
+	int lineByte = (biBitCount * m_width + 31) / 32 * 4;
 
 	if (biBitCount == 8) {
+		// 256色
 		m_channels = 1;
-		//		读取调色板
+		// 读取调色板
 		Quard = (RGBQuad*)malloc(sizeof(RGBQuad) * 256);
 		file.Read(Quard, 256 * sizeof(RGBQuad));
-		posi = file.Seek(posi + 256 * sizeof(RGBQuad), CFile::begin);
+		//posi = file.Seek(posi + 256 * sizeof(RGBQuad), CFile::begin);
 
-		//_colorAt = _color1_;
 		_colorAt = std::bind(&DIB1::_color1_, this, std::placeholders::_1, std::placeholders::_2);
+		//读取图片内容
+		_read_bmp_(file, lineByte);
+	}
+	else if (biBitCount == 4) {
+		m_channels = 1;
+		Quard = (RGBQuad*)malloc(sizeof(RGBQuad) * (1 << biBitCount));
+		file.Read(Quard, sizeof(RGBQuad) * (1 << biBitCount));
+		//posi = file.Seek(posi + sizeof(RGBQuad) * (long)(1 << biBitCount), CFile::begin);
+
+		_colorAt = std::bind(&DIB1::_color4_, this, std::placeholders::_1, std::placeholders::_2);
+		//读取图片内容
+		_read_bmp_(file, lineByte);
+	}
+	else if (biBitCount == 1) {
+		m_channels = 1;
+		Quard = (RGBQuad*)malloc(sizeof(RGBQuad) * (1 << biBitCount));
+		file.Read(Quard, sizeof(RGBQuad) * (1 << biBitCount));
+		//posi = file.Seek(posi + sizeof(RGBQuad) * (long)(1 << biBitCount), CFile::begin);
+		//file.Seek(-1, CFile::current);
+		_colorAt = std::bind(&DIB1::_color5_, this, std::placeholders::_1, std::placeholders::_2);
+		//读取图片内容
+		_read_bmp_(file, lineByte);
 	}
 	else if (biBitCount == 24) {
+		// 24位真彩色
 		m_channels = 3;
 		Quard = nullptr;
-		lineByte *= 3;
 
 		//_colorAt = _color3_;
 		_colorAt = std::bind(&DIB1::_color3_, this, std::placeholders::_1, std::placeholders::_2);
+		//读取图片内容
+		_read_bmp_(file, lineByte);
 	}
-	//读取图片内容
-	_read_bmp_(file, lineByte);
 	file.Close();
 }
 
@@ -53,13 +76,13 @@ bool DIB1::Write(const CString& fileName) {
 	VERIFY(file.Open(fileName, CFile::modeCreate | CFile::modeWrite));
 
 	try {
-		UINT lineByte = (m_width + 3) / 4 * 4 * m_channels;
+		UINT lineByte = (m_BMPInfoHeader.biBitCount * m_width + 31) / 32 * 4;
 		unsigned char* dataBuff = (unsigned char*)malloc(m_height * lineByte);
 		for (int index = 0; index < m_height; index++)
 			memcpy(dataBuff + (m_height - 1 - index) * lineByte, m_BMPdata + index * lineByte, lineByte);
 		file.Write(&m_BMPFileHeader, sizeof(m_BMPFileHeader));
 		file.Write(&m_BMPInfoHeader, sizeof(m_BMPInfoHeader));
-		if(Quard != nullptr) file.Write(Quard, sizeof(RGBQuad) * 256);
+		if(Quard != nullptr) file.Write(Quard, sizeof(RGBQuad) * (1 << m_BMPInfoHeader.biBitCount));
 		file.Write(dataBuff, m_height * lineByte);
 		free(dataBuff);
 	}
@@ -76,6 +99,7 @@ bool DIB1::Write(const CString& fileName) {
 void DIB1::_read_bmp_(CFile& file, int lineByte) {
 	m_BMPdata = (unsigned char*)malloc(lineByte * m_height);
 	unsigned char* dataBuff = (unsigned char*)malloc(lineByte * m_height);
+	//file.Read(m_BMPdata, lineByte * m_height);
 	file.Read(dataBuff, lineByte * m_height);
 	for (int index = 0; index < m_height; index++)
 		memcpy(m_BMPdata + index * lineByte, dataBuff + (m_height - 1 - index) * lineByte, lineByte);
@@ -85,7 +109,23 @@ void DIB1::_read_bmp_(CFile& file, int lineByte) {
 
 inline COLORREF DIB1::_color1_(int x, int y) const {
 	unsigned char* tmp = m_BMPdata + m_width * x + y;
-	return RGB(Quard[*tmp].rgbRed, Quard[*tmp].rgbRed, Quard[*tmp].rgbRed);// Quard[*tmp].rgbGreen, Quard[*tmp].rgbBlue
+	auto test = Quard[*tmp];
+	return RGB(Quard[*tmp].rgbRed, Quard[*tmp].rgbGreen, Quard[*tmp].rgbBlue);// Quard[*tmp].rgbGreen, Quard[*tmp].rgbBlue
+}
+
+inline COLORREF DIB1::_color4_(int x, int y) const {
+	unsigned char* tmp = m_BMPdata + m_width * x/2 + y/2;
+	int test = y%2?(*tmp >> 4):(*tmp & 0x0f);
+	return RGB(Quard[test].rgbRed, Quard[test].rgbGreen, Quard[test].rgbBlue);// Quard[*tmp].rgbGreen, Quard[*tmp].rgbBlue
+}
+
+inline COLORREF DIB1::_color5_(int x, int y) const {
+	unsigned char* tmp = m_BMPdata + (m_width + 31) / 32 * 4 * x + y / 8;
+	int test = (*tmp & (0x01 << (7 - y % 8))) >> (7 - y % 8);
+	//int test = *tmp << (7 - y % 8);
+	//test = test >> 7;
+	test &= 0x01;
+	return RGB(Quard[test].rgbRed, Quard[test].rgbGreen, Quard[test].rgbBlue);// Quard[*tmp].rgbGreen, Quard[*tmp].rgbBlue
 }
 
 inline COLORREF DIB1::_color3_(int x, int y) const {
--- MFCApplication2/MyDIB.h
+++ MFCApplication2/MyDIB.h
@@ -4,6 +4,7 @@
 #include<string>
 #include<functional>
 #include<cstdlib>
+#include<vector>
 
 #pragma pack(1)
 struct BMPFileHeader
@@ -14,6 +15,7 @@ struct BMPFileHeader
 	unsigned short bfReserved2;	//预保留位
 	unsigned long  bfOffBits;	//图像数据区的起始位置
 };
+#pragma pack()
 
 struct BMPInfoHeader
 {
@@ -32,27 +34,15 @@ struct BMPInfoHeader
 
 struct RGBQuad
 {
-	unsigned char rgbBlue;
-	unsigned char rgbGreen;
-	unsigned char rgbRed;
-	unsigned char rgbReserved;
+	BYTE rgbBlue;
+	BYTE rgbGreen;
+	BYTE rgbRed;
+	BYTE rgbReserved;
 };
-#pragma pack()
 
 class DIB1
 {
-private:
-	BMPFileHeader m_BMPFileHeader;	
-	BMPInfoHeader m_BMPInfoHeader;
-	RGBQuad* Quard;
-	int m_width, m_height, real_size, m_channels;
-	unsigned char* m_BMPdata;
-
-	void _read_bmp_(CFile& file, int lineByte);
-	//typedef COLORREF (DIB1::*color_func)(int, int) const;
 	typedef std::function<COLORREF (int, int)> color_func;
-	COLORREF _color1_(int x, int y) const;
-	COLORREF _color3_(int x, int y) const;
 public:
 	~DIB1();
 
@@ -62,5 +52,20 @@ public:
 	int height() const { return m_height; }
 	unsigned char* data() { return m_BMPdata; }
 	color_func _colorAt;
+private:
+	BMPFileHeader m_BMPFileHeader;	
+	BMPInfoHeader m_BMPInfoHeader;
+	RGBQuad* Quard;
+	int m_width, m_height, real_size;
+	int m_channels;	// 图像通道数
+	//std::vector<unsigned char> BMPdata_vec;
+	unsigned char* m_BMPdata;
+
+	void _read_bmp_(CFile& file, int lineByte);
+	//typedef COLORREF (DIB1::*color_func)(int, int) const;
+	COLORREF _color1_(int x, int y) const;
+	COLORREF _color3_(int x, int y) const;
+	COLORREF _color4_(int x, int y) const;
+	COLORREF _color5_(int x, int y) const;
 };
 
